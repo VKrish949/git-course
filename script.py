ce65244b0e4e3e585f4119c463ec4b0aa1e2e4e5
VKrish949 <lovelykrish949@gmail.com>
2025-10-24 14:46:58 +0530

    Initial commit

---
+++ script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""/c:/Project/GIT-COURSE/github/git-made-easy/script.py
+Simple demo script that prints a message.
+"""
+
+def main():
+    print("This is my demo project")
+
+if __name__ == "__main__":
+    main()
